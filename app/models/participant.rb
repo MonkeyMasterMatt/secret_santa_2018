2756f5ac740d0b268980ae52001224200f496dba
Louis Masset <lmasset16@gmail.com>
2018-10-21 19:03:04 +0200

    set up participants routes, model functions and list on the home page

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,2 +1,17 @@
 class Participant < ApplicationRecord
+  def index
+    @participants = Participant.all
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_participant
+    @participant = Participant.find(params[:id])
+  end
 end
