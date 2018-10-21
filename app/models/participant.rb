6256dea2156bb848e3b4658751664eef102a870a
Louis Masset <lmasset16@gmail.com>
2018-10-22 01:19:01 +0200

    participants model and list

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,17 +1,4 @@
 class Participant < ApplicationRecord
-  def index
-    @participants = Participant.all
-  end
-
-  def create
-  end
-
-  def destroy
-  end
-
-  private
-
-  def set_participant
-    @participant = Participant.find(params[:id])
-  end
+  validates :name, presence: true
+  validates :email, presence: true
 end
